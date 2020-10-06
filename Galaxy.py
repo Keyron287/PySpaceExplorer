c7ebbcfc06a515300d08011de1be2c88f92cc037
Keyron287 <epiclemonwizard@gmail.com>
2020-10-06 12:16:47 +0200

    Creato placeholders di Galaxy e Space_System

---
+++ Galaxy.py
@@ -0,0 +1,11 @@
+from typing import List
+
+import Space_System
+
+
+class Galaxy:
+
+    galaxy_map: List[Space_System] = None
+
+    def generate_system_connections(self, start_system):
+        pass
---
+++ Space_System.py
@@ -0,0 +1,17 @@
+
+
+class Space_system:
+
+    def __init__(self):
+        self.entities = []
+        self.connections = []
+        self.__generate()
+
+    def __generate(self):
+        pass
+
+    def enter_system(self, entity):
+        pass
+
+    def exit_system(self, entity):
+        pass
