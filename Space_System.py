caa96228d7af8cd67f2dfe967e40048e136089a7
Keyron287 <epiclemonwizard@gmail.com>
2020-10-08 17:33:07 +0200

    Update di space_System con sistema senza matrice

--- Space_System.py
+++ Space_System.py
@@ -1,6 +1,6 @@
 from math import floor
 from random import randrange, random
-from typing import List
+from typing import List, Tuple
 
 from Entity.Celestial_bodies import Star, Planet, Moon, Asteroids, Celestial_body
 from Entity.Space_Entity import Coordinate, Space_entity
@@ -9,22 +9,17 @@ from Entity.Space_Entity import Coordinate, Space_entity
 class Space_system:
 
     def __init__(self):
-        self.map = {
-            # planet : [[landed], [orbit], [space]]
-        }
+        self.planets: List[Celestial_body] = []
+        self.entities: List[Space_entity] = []
         self.connections: List[Space_system] = []
         self.__generate()
-
-    @property
-    def planets(self):
-        return list(self.map.keys())
 
     def __generate(self):
         bodies = randrange(1, 15)
 
         for a in range(bodies):
-            gauss = (random()+random())/2
-            gauss = floor(gauss*3.99)
+            gauss = (random() + random()) / 2
+            gauss = floor(gauss * 3.99)
             b = None
             if gauss == 0 or a == 0:
                 b = Star()
@@ -34,29 +29,28 @@ class Space_system:
                 b = Moon()
             elif gauss == 3:
                 b = Asteroids()
-            self.map[b] = [[], [], []]
+            self.planets.append(b)
 
     def enter_system(self, entity: Space_entity):
         entity.system.exit_system(entity)
-        self.map[self.planets[0]][Coordinate.Space].append(entity)
+        self.entities.append(entity)
         entity.position = (self.planets[0], Coordinate.Space)
 
     def exit_system(self, entity):
-        system = entity.system.map
-        coord = entity.position
-        system[coord[0]][coord[1]].remove(entity)
+        system = entity.system.entities
+        system.remove(entity)
 
     def flight(self, ship, ascend=None, away=None):
         old_coordinates = ship.position
         new_body: Celestial_body = old_coordinates[0]
         new_coordinate = old_coordinates[1]
-        body_index = list(self.map.keys()).index(new_body)
+        body_index = self.planets.index(new_body)
 
         if ascend is not None and ascend:
             if new_coordinate.value + 1 > Coordinate.Space.value:
                 raise Exception("Invalid position, outer space")
             else:
-                nc = Coordinate(new_coordinate.value+1)
+                nc = Coordinate(new_coordinate.value + 1)
                 if nc in ship.accessible_coordinates:
                     new_coordinate = nc
                 else:
@@ -71,16 +65,17 @@ class Space_system:
                 else:
                     raise Exception("Inaccessibile dalla nave")
         if away is not None and away and new_coordinate == Coordinate.space:
-            if body_index + 1 >= len(self.map):
+            if body_index + 1 >= len(self.planets):
                 raise Exception("Invalid position, out of system! out of bounds+")
             else:
-                new_body = self.planets[body_index+1]
+                new_body = self.planets[body_index + 1]
         elif away is not None and not away and new_coordinate == Coordinate.space:
             if body_index - 1 < 0:
                 raise Exception("Invalid position, out of system! out of bounds-")
             else:
-                new_body = self.planets[body_index-1]
+                new_body = self.planets[body_index - 1]
 
-        self.map[old_coordinates[0]][old_coordinates[1]].remove(ship)
-        self.map[new_body][new_coordinate].append(ship)
         ship.position = (new_body, new_coordinate)
+
+    def get_sector_entities(self, coord: Tuple[Celestial_body, Coordinate]) -> List[Space_entity]:
+        return filter(lambda x: x.position[0] == coord[0] and x.position[1] == coord[1], self.entities)
