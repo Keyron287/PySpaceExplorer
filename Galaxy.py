dc2b4980c7dedca93237bdc9065881a3dd9bff35
Keyron287 <epiclemonwizard@gmail.com>
2020-10-08 18:00:42 +0200

    Completati Galaxy e Space System

--- Galaxy.py
+++ Galaxy.py
@@ -1,11 +1,16 @@
+from random import randrange
 from typing import List
 
-import Space_System
+from Space_System import Space_system
 
 
 class Galaxy:
 
-    galaxy_map: List[Space_System] = None
+    galaxy_map: List[Space_system] = None
 
-    def generate_system_connections(self, start_system):
-        pass
+    def generate_system_connections(self, start_system: Space_system):
+        connections = min(randrange(0, 3), 3-len(start_system.connections))
+        for a in range(connections):
+            n = Space_system()
+            start_system.connections.append(n)
+            n.connections.append(start_system)
