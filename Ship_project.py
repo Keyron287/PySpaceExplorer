53ef149bfa849e31e17d17f59cb59685f9db289f
Keyron287 <epiclemonwizard@gmail.com>
2020-10-19 15:21:07 +0200

    Aggiornamenti

--- Ship_project.py
+++ Ship_project.py
@@ -1,16 +1,20 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Ship_project(ABC):
 
+    @abstractmethod
     def build_energy(self) -> float:
         return 1
 
+    @abstractmethod
     def needed_components(self) -> list:
         return []
 
+    @abstractmethod
     def production_time(self) -> int:
         return 1
 
+    @abstractmethod
     def produce_ship(self):
         pass
